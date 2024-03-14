2819a45205534534c8ce2d2bebd890c0f3809782
The5thAxiom <samridh.anand.paatni@gmail.com>
2024-03-14 15:20:47 +0530

    added separate endpoints for both binary and base64 encoded audio files

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import base64
 
 from flask import Flask, jsonify, request, g as app_ctx
 from flask_cors import CORS
@@ -27,6 +28,48 @@ def api_predict():
     request_data = request.get_json()
     try:
         audio_file = request_data['sample']
+        prediction = predict(audio_file)
+        return jsonify({
+            "status": "success",
+            "analysis": prediction['analysis'],
+            "responseTime": (time.perf_counter() - start_time) * 100
+        }), 200
+    except Exception:
+        return jsonify({
+            'message': 'failure',
+            "analysis": {},
+            "responseTime": (time.perf_counter() - start_time) * 100
+        }), 500
+    
+@app.route('/api/predict/binary', methods=['POST'])
+def api_predict_binary():
+    start_time = time.perf_counter()
+    audio_file = request.get_data()
+    try:
+        with open('data/temp/file_from_binary.mp3', 'wb') as file:
+            file.write(audio_file)
+        prediction = predict(audio_file)
+        return jsonify({
+            "status": "success",
+            "analysis": prediction['analysis'],
+            "responseTime": (time.perf_counter() - start_time) * 100
+        }), 200
+    except Exception:
+        return jsonify({
+            'message': 'failure',
+            "analysis": {},
+            "responseTime": (time.perf_counter() - start_time) * 100
+        }), 500
+    
+@app.route('/api/predict/base64', methods=['POST'])
+def api_predict_base64():
+    start_time = time.perf_counter()
+    request_data = request.get_json()
+    try:
+        audio_file_base64 = request_data['sample']
+        audio_file = base64.b64decode(audio_file_base64)
+        with open('data/temp/file_from_base64.mp3', 'wb') as file:
+            file.write(audio_file)
         prediction = predict(audio_file)
         return jsonify({
             "status": "success",
