27af36bc691c414369c2bc4fc7a428806b55eaa1
The5thAxiom <samridh.anand.paatni@gmail.com>
2024-03-15 23:40:13 +0530

    any errors are returned in message

--- code/main.py
+++ code/main.py
@@ -21,9 +21,9 @@ def api_index():
 
 @app.route('/api/predict', methods=['POST'])
 def api_predict():
-    start_time = time.perf_counter()
-    request_data = request.get_json()
     try:
+        start_time = time.perf_counter()
+        request_data = request.get_json()
         audio_file = request_data['sample']
         prediction = predict(audio_file)
         return jsonify({
@@ -31,51 +31,47 @@ def api_predict():
             "analysis": prediction['analysis'],
             "responseTime": (time.perf_counter() - start_time) * 100
         }), 200
-    except Exception:
+    except Exception as e:
         return jsonify({
-            'message': 'failure',
+            'message': f'failure: {e}',
             "analysis": {},
             "responseTime": (time.perf_counter() - start_time) * 100
         }), 500
     
 @app.route('/api/predict/binary', methods=['POST']) 
 def api_predict_binary():
-    start_time = time.perf_counter()
-    audio_file = request.get_data()
-    with open('data/temp/current_file.wav', 'wb') as file:
-        file.write(audio_file)
-    prediction = predict('data/temp/current_file.wav')
     try:
+        start_time = time.perf_counter()
+        audio_file = request.get_data()
+        prediction = predict(audio_file)
         return jsonify({
             "status": "success",
             "analysis": prediction['analysis'],
             "responseTime": (time.perf_counter() - start_time) * 100
         }), 200
-    except Exception:
+    except Exception as e:
         return jsonify({
-            'message': 'failure',
+            'message': f'failure: {e}',
             "analysis": {},
             "responseTime": (time.perf_counter() - start_time) * 100
         }), 500
     
 @app.route('/api/predict/base64', methods=['POST'])
 def api_predict_base64():
-    start_time = time.perf_counter()
-    request_data = request.get_json()
-    audio_file_base64 = request_data['sample']
-    audio_file = base64.b64decode(audio_file_base64)
-    with open('data/temp/file_from_base64.mp3', 'wb') as file:
-        file.write(audio_file)
-    prediction = predict(audio_file)
     try:
+        start_time = time.perf_counter()
+        request_data = request.get_json()
+        audio_file_base64 = request_data['sample']
+        audio_file = base64.b64decode(audio_file_base64)
+        prediction = predict(audio_file)
         return jsonify({
             "status": "success",
             "analysis": prediction['analysis'],
             "responseTime": (time.perf_counter() - start_time) * 100
         }), 200
-    except Exception:
+    except Exception as e:
         return jsonify({
-            'message': 'failure',
+            'message': f'failure: {e}',
             "analysis": {},
             "responseTime": (time.perf_counter() - start_time) * 100
         }), 500
@@ -84,5 +80,5 @@ if __name__ == "__main__":
     app.run(
         host='127.0.0.1',
         port=5000,
-        debug=True
+        debug=True,
     )
