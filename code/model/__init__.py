421b09c6d693b5712ba4f876e06581cf7e88e0f8
The5thAxiom <samridh.anand.paatni@gmail.com>
2024-03-16 23:53:08 +0530

    corrected emotional detection code; bgnoise.py renamed to __init__.py

--- code/model/__init__.py
+++ code/model/__init__.py
@@ -1,9 +1,11 @@
 import random
-from model.bgnoise.bgnoise import detectBgNoise
+from model.bgnoise import detectBgNoise
+from model.emotion import emotional_analysis
 
 def predict(audio_file):
     # this dict could have more parameters which the frontend could show
     # right now this returns a random bool
+    transcript = 'i hate google cloud'
     return {
         "analysis": {
             "detectedVoice": True,
@@ -13,7 +15,7 @@ def predict(audio_file):
                 "humanProbability": 12
             },
             "additionalInfo": {
-                "emotionalTone": "neutral",
+                "emotionalTone": emotional_analysis(transcript),
                 "backgroundNoiseLevel": detectBgNoise(audio_file),
                 # "language": "hindi" | "english" | ...,
                 # "accent": "indian" | "american" | ...,
---
+++ code/model/emotion/__init__.py
@@ -0,0 +1,24 @@
+from requests import post
+
+def emotional_analysis(text):
+  print('run emotion')
+  api_key = 'AIzaSyAD-oOA-fxuBrZOvMovt-AV92XYIRuOoBI' # should be stored as an environment variable but raat 12 baje ye nahi hoga hamse
+  url = f'https://language.googleapis.com/v1/documents:analyzeSentiment?key={api_key}'
+  payload = {
+    "document":
+      {
+        "type": "PLAIN_TEXT",
+        "content": text
+      },
+      "encodingType": "UTF8"
+    }
+  res = post(url, json=payload)
+  data = res.json()
+  score = data['documentSentiment']['score']
+  
+  if score < -.7:
+    return 'negative'
+  elif score > .7:
+    return 'positive'
+  else:
+    return 'neutral'
